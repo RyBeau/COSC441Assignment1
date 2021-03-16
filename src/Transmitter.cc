881a661d485cafd1b40c52128281e07f6cd6235d
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-16 21:22:32 +1300

    Setup basic files

--- src/Transmitter.cc
+++ src/Transmitter.cc
@@ -1,28 +1,40 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
 #include "Transmitter.h"
+#include "PacketRecord_m.h"
 
 Define_Module(Transmitter);
 
 void Transmitter::initialize()
 {
-    // TODO - Generated method body
+    numberOverheadBits = 100;
+    numberUserBits = 1000;
+    outGateId = findGate("transmitGate");
+    inGateId = findGate("receiveGate");
 }
 
 void Transmitter::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    if (msg->arrivedOn(inGateid)) {
+
+        PacketRecord *packetRecord = new PacketRecord;
+
+        EV << "Transmitter::handleMessage: generating a new packet record at time "
+                << simTime() << " with sequence number "
+                << sequenceNumber << " and with packet size";
+
+        packetRecord->setSequenceNumber(sequenceNumber++);
+        packetRecord->setOvhdBits(numberOverheadBits);
+        packetRecord->setUserBits(numberUserBits);
+        packetRecord->setErrorFlag(false);
+
+        send(packetRecord, outGateId);
+        delete msg;
+
+    } else {
+        error("Transmitter:: Received Unexpected Message");
+    }
+}
+
+void Transmitter::~Transmitter()
+{
+    // TODO
 }
--- src/Transmitter.h
+++ src/Transmitter.h
@@ -1,18 +1,3 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
 #ifndef __COSC441ASSIGNMENT1_TRANSMITTER_H_
 #define __COSC441ASSIGNMENT1_TRANSMITTER_H_
 
@@ -20,14 +5,19 @@
 
 using namespace omnetpp;
 
-/**
- * TODO - Generated class
- */
 class Transmitter : public cSimpleModule
 {
-  protected:
+  public:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
+    ~Transmitter();
+
+  private:
+    int64_t numberOverheadBits;
+    int64_t numberUserBits;
+    int outGateId;
+    int inGateId;
+    int sequenceNumber = 0;
 };
 
 #endif
